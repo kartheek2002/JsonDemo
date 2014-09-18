1c2cc42f70346809cc3930d0f1ee7812aa993406
kartheek2002 <halo55@gmail.com>
2014-09-18 07:46:12 -0500

    Json Working

---
+++ src/jsondemo/DataTableServlet.java
@@ -0,0 +1,51 @@
+package jsondemo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation to pass JSON to JSP
+ */
+
+public class DataTableServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public DataTableServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String cluster = request.getParameter("clustername");
+		
+		PrintWriter out = response.getWriter();
+		
+		String json = "{ \"demo\":[[\"John\",\"Sam\",\"Sola\",\"Accun\"],[\"Raj\",\"Raj\",\"Sanjosh\",\"CA\"]]}";
+		System.out.println(cluster);
+		if (cluster.length() > 0 )
+		{
+			json = "{ \"demo\":[[\"John\",\"Sam\",\"Sola\",\"Accun\"],[\"Raj\",\"Raj\",\"Sanjosh\",\"CA\"],[\"Raj\",\"Raj\",\"Sanjosh\",\"CA\"],[\"Raj\",\"Raj\",\"Sanjosh\",\"CA\"]]}";
+		}
+		
+		out.println(json);
+	}
+}
